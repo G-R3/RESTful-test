169d0b8998edad894f175b569f6578524d41f4d2
Gerardo <gr261212@gmail.com>
2021-05-22 19:59:07 -0400

    Create a comment

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const { v4: uuid } = require("uuid");
 const data = require("./data.json");
 const PORT = 3000;
 
@@ -10,8 +11,20 @@ app.set("view engine", "ejs");
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+// view comments
 app.get("/comments", (req, res) => {
   res.render("comments/index", { data });
+});
+
+// create comment
+app.get("/comments/create", (req, res) => {
+  res.render("comments/create");
+});
+
+app.post("/comments/create", (req, res) => {
+  const { name, comment } = req.body;
+  data.push({ id: uuid(), name, body: comment });
+  res.redirect("/comments");
 });
 
 app.listen(PORT, (req, res) => {
