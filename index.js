5482e649997e70c7979890cb19548a655b90b213
Gerardo <gr261212@gmail.com>
2021-05-22 21:25:20 -0400

    Update a comment route

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const { v4: uuid } = require("uuid");
+const methodOverride = require("method-override");
 const data = require("./data.json");
 const PORT = 3000;
 
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 app.set("view engine", "ejs");
 app.set("view engine", "ejs");
@@ -16,7 +18,7 @@ app.get("/comments", (req, res) => {
   res.render("comments/index", { data });
 });
 
-// create comment
+// Create a comment
 app.get("/comments/create", (req, res) => {
   res.render("comments/create");
 });
@@ -27,7 +29,7 @@ app.post("/comments/create", (req, res) => {
   res.redirect("/comments");
 });
 
-// view single comment
+// view a single comment
 app.get("/comments/:id", (req, res) => {
   const { id } = req.params;
   let foundComment = data.find((comment) => comment.id === parseInt(id));
@@ -42,6 +44,44 @@ app.get("/comments/:id", (req, res) => {
 
   res.render("comments/show", { foundComment });
 });
+
+// Update a comment
+app.get("/comments/:id/edit", (req, res) => {
+  const { id } = req.params;
+  let foundComment = data.find((comment) => comment.id === parseInt(id));
+
+  /**
+   * since newly created comments will get an ID from UUID(), they will return undefined
+   * if we try to parse the ID. This is just a temp solution to get the comment we want.
+   */
+  if (!foundComment) {
+    foundComment = data.find((comment) => comment.id === id);
+  }
+
+  res.render("comments/edit", { foundComment });
+});
+
+app.patch("/comments/:id/edit", (req, res) => {
+  const { name, email, comment } = req.body;
+  const { id } = req.params;
+  let foundComment = data.find((comment) => comment.id === parseInt(id));
+
+  /**
+   * since newly created comments will get an ID from UUID(), they will return undefined
+   * if we try to parse the ID. This is just a temp solution to get the comment we want.
+   */
+  if (!foundComment) {
+    foundComment = data.find((comment) => comment.id === id);
+  }
+
+  foundComment.name = name;
+  foundComment.email = email;
+  foundComment.body = comment;
+
+  res.redirect("/comments");
+});
+
+// Delete a Comment
 
 app.listen(PORT, (req, res) => {
   console.log(`Listening on ${PORT}`);
